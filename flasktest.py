239d7d216c22cd42fb50d0dd064569c2278f0955
Phantasmiic <joeychiu25@gmail.com>
2018-08-01 10:14:44 -0500

    finally able to transmit data via POST from client to server

--- flasktest.py
+++ flasktest.py
@@ -1,38 +1,33 @@
-from flask import Flask, render_template, request, url_for, redirect
+from flask import Flask, render_template, request, url_for, redirect, jsonify
+import json
 app = Flask(__name__)
 
 
 data = [[1,2,3],[4,5,6]]
 
-@app.route('/')
-@app.route('/index')
-def index():
-    return render_template("dropdown.html")
 
+		
 @app.route('/login', methods=['GET', 'POST'])
 def login():
 	if request.method == "POST":
-		function = request.form.get("function", None)
-		if function!=None:
-			writeData(function)
+		data = request.get_json()
+		print('this is data')
+		print(data)	
+		if data != None:
+			print('adadad')
 			
-			# if function == 'StickIn':
-			# elif function == 'StickOut':
-			# elif function == 'BoomUp':
-			# elif function == 'BoomDown':
-			
-			
-			return render_template("dropdown.html", function=function)
-	#writeData("Nothing yet")
-	#return render_template("dropdown.html")
-	return render_template("dadadada")
+			writeData(json.dumps(data))
+			return render_template("dropdown.html", car_brand = data)
+	writeData('failed')
+	return render_template("dropdown.html")
 
-
+		
 
 def writeData(data):
-	#with open('data/machine1.txt', 'w') as dataRead:
-	#	dataRead.write(data)
+	with open('data/machine1.txt', 'w') as dataRead:
+		dataRead.write(data)
 	pass
 		
 if __name__ == '__main__':
     app.run(debug=True)
+	
