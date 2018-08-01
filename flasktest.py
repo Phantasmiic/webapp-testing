0199a276c49bca5ed2f960c80fcbaa9be8c04998
Phantasmiic <joeychiu25@gmail.com>
2018-08-01 15:39:15 -0500

    moving back to work pc

--- flasktest.py
+++ flasktest.py
@@ -3,25 +3,33 @@ import json
 app = Flask(__name__)
 
 
-data = [[1,2,3],[4,5,6]]
+data = {"StickIn": 100,
+		"StickOut": 200,
+		"BoomOut":300,
+		"BoomUp":400}
 
 
 		
 @app.route('/login', methods=['GET', 'POST'])
 def login():
 	if request.method == "POST":
-		data = request.get_json()
-		print('this is data')
-		print(data)	
-		if data != None:
-			print('adadad')
-			
-			writeData(json.dumps(data))
-			return render_template("dropdown.html", car_brand = data)
-	writeData('failed')
-	return render_template("dropdown.html")
+		functionality = request.get_json()
+		# print('this is data')
+		# print(data)	
+		if functionality != None:
+			#processData(data)
+			writeData(json.dumps(functionality))
+			return render_template("dropdown.html", par_dict = 'this is retarded')
 
-		
+	writeData('failed')
+	return render_template("dropdown.html", par_dict = 'adadadad')
+
+@app.route('/processData/<data>')
+def processData(data):
+	functionality = data
+	if functionality != None:
+		return jsonify(data)
+	return jsonify('failed')
 
 def writeData(data):
 	with open('data/machine1.txt', 'w') as dataRead:
